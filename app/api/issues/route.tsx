6c6e04962c67c90330070841d87191fbc8ce48fb
Robin6799 <robin@bpoc.co.jp>
2024-10-31 17:22:02 +0800

    feat/createIssuePosting

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from './schema'
+import { prisma } from "@/prisma/client";
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = schema.safeParse(body)
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400 })
+
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description}
+    })
+
+    return NextResponse.json(newIssue, { status: 201 })
+}
