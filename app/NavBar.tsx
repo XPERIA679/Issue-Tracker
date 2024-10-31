c3abfb21d3de7735a4ba6c9cb686dceedb227aa8
Robin6799 <robin@bpoc.co.jp>
2024-10-31 14:55:14 +0800

    feat/navbar: Show active current page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import Link from 'next/link'
 import React from 'react'
 import { BsBugFill } from 'react-icons/bs'
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const NavBar = () => {
+  const currentPage = usePathname();
+  console.log(currentPage);
+
   const links = [
     { label: 'Dashboard', href: '/'},
     { label: 'Issues', href: '/issues'},
@@ -14,7 +21,11 @@ const NavBar = () => {
       <ul className='flex space-x-6'>
         {links.map(link => 
         <Link key={link.href} 
-        className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+        className={classnames({
+            'text-zinc-900': link.href === currentPage,
+            'text-zinc-500': link.href !== currentPage,
+            'hover:text-zinc-800 transition-colors': true
+        })} 
         href={link.href}>{link.label}</Link>)}
       </ul>
     </nav>
