9ee4457fcd61d3ad5ef03cb39141f88034a8755c
Robin6799 <robin@bpoc.co.jp>
2024-11-04 10:49:39 +0800

    feat/build-issue-page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import '@radix-ui/themes/styles.css';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
-import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,14 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Theme>
-        <body className={inter.className}>
+      <body className={inter.className}>
+        <Theme>
           <NavBar />
-          <main>
+          <main className='p-5'>
             {children}
           </main>
-        </body>
-      </Theme>
+        </Theme>
+      </body>
     </html>
   )
 }
