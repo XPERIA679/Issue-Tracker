6ed5170bfb011e38c6169bdd465ab6c1cfa382df
Robin6799 <robin@bpoc.co.jp>
2024-11-05 09:45:29 +0800

    change error message to reusable component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import validationschema from '@/app/validationSchema';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof validationschema>;
 
@@ -44,9 +45,9 @@ const NewsIssuePage = () => {
       <TextField.Root>
           <TextField.Input placeholder="Title" {...register('title')}/>
       </TextField.Root>
-      {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
       <Controller name='description' control={control} render={({ field }) => <SimpleMDE placeholder='Description' {...field}/>} />
-      {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+      <ErrorMessage>{errors.description?.message}</ErrorMessage>
       <Button>Submit Issue</Button>
       </form>
     </div>
