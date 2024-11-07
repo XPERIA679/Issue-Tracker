fad49b899944b0aaada316e5a049a3cfe7ea7481
Robin6799 <robin@bpoc.co.jp>
2024-11-07 17:50:44 +0800

    Add TryCatch on delete

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,10 +3,23 @@ import { Button, AlertDialog, Flex } from '@radix-ui/themes';
 import { TrashIcon } from '@radix-ui/react-icons';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number}) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+  const deleteIssue =  async () => {
+    try {
+        await axios.delete('/api/issues/' + issueId)
+        router.push('/issues');
+        router.refresh();
+    } catch (error) {
+        setError(true);
+    }
+}
+
   return (
+    <>
     <AlertDialog.Root>
       <AlertDialog.Trigger>
         <Button color='red'>
@@ -25,15 +38,23 @@ const DeleteIssueButton = ({ issueId }: { issueId: number}) => {
             <Button variant="soft" color="gray">Cancel</Button>
           </AlertDialog.Cancel>
           <AlertDialog.Action>
-            <Button variant="solid" color="red" onClick={ async () => {
-                await axios.delete('/api/issues/' + issueId)
-                router.push('/issues');
-                router.refresh();
-            }}>Delete Issue</Button>
+            <Button variant="solid" color="red" onClick={deleteIssue}>Delete Issue</Button>
           </AlertDialog.Action>
         </Flex>
       </AlertDialog.Content>
     </AlertDialog.Root>
+
+    <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+            <AlertDialog.Title>Error</AlertDialog.Title>
+            <AlertDialog.Description>Issue could not be deleted.</AlertDialog.Description>
+            <Flex justify="end"><Button mt="2"  color="gray" variant="soft"
+             onClick={ () => {
+                setError(false)
+             }}>OK</Button></Flex>
+        </AlertDialog.Content>
+    </AlertDialog.Root>
+    </>
   )
 }
 
