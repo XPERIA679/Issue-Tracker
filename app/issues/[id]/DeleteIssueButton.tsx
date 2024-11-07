5cac7bf4ce71e9bad40e81477095a2b287d42122
Robin6799 <robin@bpoc.co.jp>
2024-11-07 16:45:32 +0800

    Add Confirmation Dialogue to Button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,15 +1,32 @@
-import Link from 'next/link';
-import { Button } from '@radix-ui/themes';
+'use client';
+import { Button, AlertDialog, Flex } from '@radix-ui/themes';
 import { TrashIcon } from '@radix-ui/react-icons';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number}) => {
   return (
-    <Button color='red'>
-      <TrashIcon />
-      <Link href={`/issues/${issueId}/delete`}>
-      </Link>
-      Delete
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>
+        <TrashIcon />
+          Delete
+        </Button>
+      </AlertDialog.Trigger>  
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description>
+            Are you sure? This will delete the issue and will not be accessible anymore.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
