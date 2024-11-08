84b91b0c58af6a560c1c93f9d47f1f452966d7d1
Robin6799 <robin@bpoc.co.jp>
2024-11-08 13:58:25 +0800

    Configure next-auth and prisma adapter for google provider login

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 import { Container, Theme, ThemePanel } from '@radix-ui/themes'
+// import AuthProvider from './auth/Provider';
 
 const inter = Inter({ subsets: ['latin'], variable: "--font-inter" })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
+        {/* <AuthProvider> */}
         <Theme appearance="light" accentColor="violet">
           <NavBar />
           <main className='p-5'>
@@ -29,6 +31,7 @@ export default function RootLayout({
             </Container>
           </main>
         </Theme>
+        {/* </AuthProvider> */}
       </body>
     </html>
   )
