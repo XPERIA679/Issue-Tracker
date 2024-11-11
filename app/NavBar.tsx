154375e0b316af2509787e38703d45563827cd7e
Robin6799 <robin@bpoc.co.jp>
2024-11-11 09:52:58 +0800

    Separate page links and user links in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,40 +7,46 @@ import { usePathname } from 'next/navigation';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
 import { Spinner } from './components';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const NavBar = () => {
   const currentPage = usePathname();
-  const { status, data:session } = useSession();
+  const { status, data: session } = useSession();
 
   const links = [
-    { label: 'Dashboard', href: '/'},
-    { label: 'Issues', href: '/issues'},
-  ]  
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ]
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href="/"><BsBugFill /></Link>
-      <ul className='flex space-x-6'>
-        {links.map((link) => (
-        <li key={link.href} >
-        <Link 
-        className={classnames({
-            'text-zinc-900': link.href === currentPage,
-            'text-zinc-500': link.href !== currentPage,
-            'hover:text-zinc-800 transition-colors': true
-        })} 
-        href={link.href}>{link.label}</Link></li>))}
-      </ul>
-      <Box>
-        { status === 'loading' && <Spinner /> }
-        { status === 'authenticated' && 
-        <div>
-          { session.user!.name }
-          <Link href="/api/auth/signout" className='ml-5'>Sign Out</Link>
-        </div>}
-        { status === 'unauthenticated' && <Link href="/api/auth/signin" className='mr-5'> Sign In </Link> }
-      </Box>
+    <nav className='border-b mb-5 px-5 py-3'>
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/" className=''><BsBugFill /></Link>
+            <ul className='flex space-x-6'>
+              {links.map((link) => (
+                <li key={link.href} >
+                  <Link
+                    className={classnames({
+                      'text-zinc-900': link.href === currentPage,
+                      'text-zinc-500': link.href !== currentPage,
+                      'hover:text-zinc-800 transition-colors': true
+                    })}
+                    href={link.href}>{link.label}</Link></li>))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'loading' && <Spinner />}
+            {status === 'authenticated' &&
+              <div>
+                {session.user!.name}
+                <Link href="/api/auth/signout" className='ml-5'>Sign Out</Link>
+              </div>}
+            {status === 'unauthenticated' && <Link href="/api/auth/signin" className='mr-5'> Sign In </Link>}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   )
 }
