4687d558265ca104a57fed65ccbc729743f6661c
Robin6799 <robin@bpoc.co.jp>
2024-11-12 16:14:12 +0800

    Filter Issues Realtime

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -1,18 +1,24 @@
 'use client';
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
+const statuses: { label: string, value?: Status }[] = [
+    { label: 'All' },
+    { label: 'Open', value: 'OPEN' },
+    { label: 'Closed', value: 'CLOSED' },
+    { label: 'In Progress', value: 'IN_PROGRESS' },
+]
+    
 const IssueStatusFilter = () => {
-  const statuses: { label: string, value?: Status }[] = [
-      { label: 'All' },
-      { label: 'Open', value: 'OPEN' },
-      { label: 'Closed', value: 'CLOSED' },
-      { label: 'In Progress', value: 'IN_PROGRESS' },
-  ]
-      
+   const router = useRouter(); 
+
   return (
-    <Select.Root>
+    <Select.Root onValueChange={(status) => {
+        const query = status ? `?status=${status}` : '';
+        router.push('/issues' + query)
+    }}>
         <Select.Trigger placeholder="Filter"/>
         <Select.Content>
             <Select.Group>
