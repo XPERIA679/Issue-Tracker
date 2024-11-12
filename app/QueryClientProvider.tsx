4432efd385efb9f990fdcecbb9ef84d2d2a936bd
Robin6799 <robin@bpoc.co.jp>
2024-11-12 10:06:10 +0800

    Setup React Query

---
+++ app/QueryClientProvider.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider as ReactQueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
+
+const queryClient = new QueryClient();
+
+const QueryClientProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <div>
+      <ReactQueryClientProvider client={queryClient}>
+        {children}
+      </ReactQueryClientProvider>
+    </div>
+  )
+}
+
+export default QueryClientProvider
