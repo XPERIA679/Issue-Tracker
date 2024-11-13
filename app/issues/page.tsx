d4bab7db01f2a77d38c8057f989e73acb4d5bc9d
Robin6799 <robin@bpoc.co.jp>
2024-11-13 10:17:33 +0800

    Add sorting columns

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,11 +3,24 @@ import { Table } from '@radix-ui/themes';
 import { prisma } from '@/prisma/client';
 import IssueActionBar from './IssueActionBar';
 import { Link, IssueStatusBadge} from '@/app/components';
-import { Status } from '@prisma/client';
+import NextLink from 'next/link';
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 
 const IssuesPage = async ({ searchParams, }: { 
-  searchParams: { status : Status };
+  searchParams: { status : Status, orderBy: keyof Issue };
 }) => {
+
+  const columns: { 
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    
+    { label: 'Issue', value: 'title'},
+    { label: 'Status', value: 'status', className: 'hidden md:table-cell'},
+    { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell'}
+  ]
 
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
@@ -26,9 +39,14 @@ const IssuesPage = async ({ searchParams, }: {
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+          {columns.map(column => (
+            <Table.ColumnHeaderCell key={column.value}>
+              <NextLink href={{
+                query: { ...searchParams, orderBy: column.value }
+              }}>{column.label}</NextLink>
+              {column.value === searchParams.orderBy && <ArrowUpIcon className='inline'/> }
+            </Table.ColumnHeaderCell>
+          ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
