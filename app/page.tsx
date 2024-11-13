1db8ee75dcbbef5ede6daa4badc737b99dd0f700
Robin6799 <robin@bpoc.co.jp>
2024-11-13 14:40:36 +0800

    Create Pagination function

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@radix-ui/themes";
 import { Pagination } from "./components";
 
-export default function Home() {
+export default function Home({ searchParams }: { searchParams: {page: string}}) {
   return (
     // <div>こんにちは世界</div>
-    <Pagination itemCount={100} pageSize={10} currentPage={10}/>
+    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
   );
 }
