c4904d9d234a03c409e48e88b9e7a0957756ab83
Robin6799 <robin@bpoc.co.jp>
2024-11-13 14:02:39 +0800

    Create design of pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@radix-ui/themes";
+import { Pagination } from "./components";
 
 export default function Home() {
   return (
-    <div>こんにちは世界</div>
+    // <div>こんにちは世界</div>
+    <Pagination itemCount={100} pageSize={10} currentPage={10}/>
   );
 }
