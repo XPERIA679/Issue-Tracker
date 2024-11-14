21f893b13430284a9bcb5f383577b439cefdeb9a
Robin6799 <robin@bpoc.co.jp>
2024-11-14 10:51:42 +0800

    Add dashboard with latest issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@radix-ui/themes";
-import { Pagination } from "./components";
+import LatestIssues from "./LatestIssues";
 
-export default function Home({ searchParams }: { searchParams: {page: string}}) {
+export default function Home() {
   return (
-    // <div>こんにちは世界</div>
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
+    <LatestIssues />
   );
 }
