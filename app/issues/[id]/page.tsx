4a0e92844547ce86bf2485e58682525f9ca87473
Robin6799 <robin@bpoc.co.jp>
2024-11-14 16:42:17 +0800

    Add react caching

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,19 +7,19 @@ import authOptions from '@/app/auth/authOptions';
 import { Box, Flex, Grid } from '@radix-ui/themes';
 import DeleteIssueButton from './DeleteIssueButton';
 import AssigneeSelect from './AssigneeSelect';
-import { describe } from 'node:test';
+import { cache } from 'react';
 
 
 interface Props {
   params: { id: string }
 }
 
+const fetchUser = cache((issueId: number) => prisma.issue.findUnique({ where: { id: issueId } }));
+
 const IssueDetailsPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions)
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) }
-  })
+  const issue = await fetchUser(parseInt(params.id))
 
   if (!issue)
     notFound();
@@ -41,7 +41,7 @@ const IssueDetailsPage = async ({ params }: Props) => {
 }
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) }})
+  const issue = await fetchUser(parseInt(params.id))
 
   return {
     title: issue?.title,
